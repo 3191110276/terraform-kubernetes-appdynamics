033496984d2e22fdf68449fc090d4ff31d7c1fd9
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-20 22:19:18 +0200

    Update main.tf

--- modules/dbcollector/main.tf
+++ modules/dbcollector/main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 1.10.0"
+      version = ">= 2.0.2"
     }
     appdynamics = {
       source = "3191110276/appdynamics"
