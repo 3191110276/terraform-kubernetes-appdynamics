8354ec9957e8af265bc49a3635f54ce98b5f1c34
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-20 18:28:26 +0200

    Update main.tf

--- modules/clusteragent/main.tf
+++ modules/clusteragent/main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0.2"
+      version = ">= 1.10.0"
     }
     helm = {
       source  = "hashicorp/helm"
